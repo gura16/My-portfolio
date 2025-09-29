c5cb75900220db934bc38dd5aa11a1a5e5e48365
guramikvakhadze <gkvaxadze@gmail.com>
2025-09-29 22:55:54 +0100

    add headerfolder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { createGlobalStyle } from "styled-components";
+import Header from "./header";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: rgb(1, 12, 21);
+  }
+`;
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/header.tsx
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+function Header() {
+  return <Headercomponent></Headercomponent>;
+}
+
+export default Header;
+
+const Headercomponent = styled.header``;
